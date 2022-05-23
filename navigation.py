9b6dae8a13cc49433007b5d43ccad37fa9fd24a9
ayaanza29 <91754746+ayaanza29@users.noreply.github.com>
2022-05-23 15:02:19 -0400

    html work

---
+++ navigation.py
@@ -0,0 +1,33 @@
+from flask import Flask, flash, request, render_template
+
+UPLOAD_FOLDER = "C:/Users/Zuhayr/Desktop/Zuhayr_Web_Data"
+app = Flask(__name__, static_url_path='/static')
+app.config["Upload_Folder"] = UPLOAD_FOLDER
+
+############################ general ############################
+
+@app.route("/")
+def dashboard():
+    return render_template("general/dashboard.html")
+
+@app.route("/about_us")
+def about_us():
+    return render_template("general/about_us.html")
+
+@app.route("/account")
+def account():
+    return render_template("general/account.html")
+
+@app.route("/help")
+def help():
+    return render_template("general/help.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("general/settings.html")
+
+############################ job_specific ############################
+
+@app.route("/upload_data")
+def upload_data():
+    return render_template("job_specific/upload_data.html")
