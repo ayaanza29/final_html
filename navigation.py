0e2458a99e55a67713dc3541f4870ee8edb36f8e
Zuhayr <ayaanza29@gmail.com>
2022-05-27 18:11:05 -0400

    style

--- navigation.py
+++ navigation.py
@@ -6,28 +6,39 @@ app.config["Upload_Folder"] = UPLOAD_FOLDER
 
 ############################ general ############################
 
+
 @app.route("/")
-def dashboard():
+def initial():
     return render_template("general/dashboard.html")
 
 @app.route("/about_us")
 def about_us():
-    return render_template("general/about_us.html")
+    return render_template("general_about_us.html")
 
 @app.route("/account")
 def account():
-    return render_template("general/account.html")
+    return render_template("general_account.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("general/dashboard.html")
 
 @app.route("/help")
 def help():
-    return render_template("general/help.html")
+    return render_template("general_help.html")
 
 @app.route("/settings")
 def settings():
-    return render_template("general/settings.html")
+    return render_template("general_settings.html")
 
 ############################ job_specific ############################
+
 
 @app.route("/upload_data")
 def upload_data():
     return render_template("job_specific/upload_data.html")
+
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.run(port = 5000, debug = True)
+    
